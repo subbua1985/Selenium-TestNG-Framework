5a95ac584f56b4e4e05e3237fcc3e96029348971
subbua1985 <a.subbu1985@gmail.com>
2022-05-17 10:59:35 +1200

    added email in Registration.Java

--- src/main/java/com/mylotto/qa/base/TestBase.java
+++ src/main/java/com/mylotto/qa/base/TestBase.java
@@ -1,6 +1,8 @@
 package com.mylotto.qa.base;
 
 import java.io.FileInputStream;
+
+
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
--- src/test/java/com/mylotto/qa/testcases/RegistrationMyLotto.java
+++ src/test/java/com/mylotto/qa/testcases/RegistrationMyLotto.java
@@ -1,5 +1,5 @@
 /*
- * Author: Subramani Ayyanar.
+ * Author: Subramani.Ayyanar@lottonz.co.nz.
  */
 package com.mylotto.qa.testcases;
 
